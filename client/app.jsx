5601d2758ff2dbd838b57da212cf664e1c0d19b4
changhyun yoo <chyoo0428@gmail.com>
2022-07-05 22:02:38 +0000

    successfully retrieve api data filter location

--- client/app.jsx
+++ client/app.jsx
@@ -3,17 +3,20 @@ import parseRoute from './lib/parseRoute';
 import GetAllAnimal from './pages/all-animals';
 import Home from './pages/home';
 import GetDetails from './pages/detail';
+import GetRandom from './pages/random';
 // eslint-disable-next-line
 const { getAccessToken } = require('./component/petfinder');
 
 export default function App() {
   const [route, setRoute] = useState(parseRoute(window.location.hash));
   const { path } = route;
+  const location = window.location.hash;
   useEffect(() => {
+    getAccessToken();
     window.addEventListener('hashchange', () => {
       setRoute(parseRoute(window.location.hash));
     });
-  }, [window.location.hash]);
+  }, [location]);
   const renderPage = () => {
     if (path === '') {
       return <Home/>;
@@ -23,6 +26,9 @@ export default function App() {
     }
     if (path === 'recent') {
       return <GetAllAnimal/>;
+    }
+    if (path === 'random') {
+      return <GetRandom location={route.params.get('location')}/>;
     }
   };
   return renderPage();
