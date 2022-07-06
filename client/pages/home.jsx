bbeb18cd2010710faa2bca97fa9e6ffcb25507d6
changhyun yoo <chyoo0428@gmail.com>
2022-07-06 10:35:34 +0000

    landing page styled

--- client/pages/home.jsx
+++ client/pages/home.jsx
@@ -7,11 +7,17 @@ export default function Home() {
     window.location.hash = `#random?location=${location}`;
   }
   return (
-    <div className="w-100 h-100 bg-main">
+    <div className="col-lg-6 col-md-10 col-sm-10 col-xs-10 h-100 mx-auto">
       <div className="d-flex flex-column h-100 justify-content-center align-items-center">
-        <div className="w-100 h-75 text-center mx-auto box-shadow border-filter">
+        <div className="mb-3">
+          <h4 className="text-main">Let&apos;s find furever home!</h4>
+        </div>
+        <div className="home-image-container">
+          <img className="object-fit" src="/furever-paws.png"></img>
+        </div>
+        <div className="w-90 h-30 mt-5 text-center mx-auto box-shadow">
           <form onSubmit={handleSubmit}>
-            <div>
+            <div className="w-100 h-30 d-flex flex-column align-items-center justify-content-around search-icon-container">
               <input
               required
               id="location"
@@ -19,13 +25,12 @@ export default function Home() {
               types="text"
               value={location}
               onChange={e => setLocation(e.target.value)}
-              placeholder="Location"
-              className="h5 mt-2"
+              placeholder="         SEARCH: LOCATION, ZIP CODE"
+                className="small w-90 main-icon-container"
               />
-            </div>
-            <div>
-              <button type="submit" className="w-75 btn btn-primary h5 text-black">Random</button>
-              <a href="#recent" className="w-75 btn btn-primary mt-5 h5 text-black">List</a>
+              <i className="fa-lg fa-solid fa-magnifying-glass"></i>
+              <button type="submit" className="text-main w-90 btn btn-primary h5 border-filter">Random</button>
+              <a href="#recent" className="text-main w-90 btn btn-primary h5 border-filter">List</a>
             </div>
           </form>
         </div>
