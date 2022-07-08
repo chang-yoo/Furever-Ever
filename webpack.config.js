2527d7747901f35867163b314974a6faef7f5c63
changhyun yoo <chyoo0428@gmail.com>
2022-07-08 04:01:54 +0000

    config chagned

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,58 @@
 require('dotenv/config');
-const Dotenv = require('dotenv-webpack');
-
 const path = require('path');
+const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
-const clientPath = path.join(__dirname, 'client/');
-const publicPath = path.join(__dirname, 'server/public/');
+const clientPath = path.join(__dirname, 'client');
+const serverPublicPath = path.join(__dirname, 'server', 'public');
+
+const isDevelopment = process.env.NODE_ENV === 'development';
 
 module.exports = {
   mode: process.env.NODE_ENV,
-  plugins: [
-    new Dotenv()
-  ],
+  entry: [
+    clientPath,
+    isDevelopment && 'webpack-hot-middleware/client?timeout=1000'
+  ].filter(Boolean),
   resolve: {
     extensions: ['.js', '.jsx']
   },
-  entry: clientPath,
   output: {
-    path: publicPath
+    path: serverPublicPath
   },
   module: {
     rules: [
       {
-        test: /\.jsx/,
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           options: {
+            presets: [
+              '@babel/preset-env'
+            ],
             plugins: [
-              '@babel/plugin-transform-react-jsx'
-            ]
+              '@babel/plugin-transform-react-jsx',
+              isDevelopment && 'react-refresh/babel'
+            ].filter(Boolean)
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
       }
     ]
   },
-  devtool: 'source-map',
-  devServer: {
-    contentBase: publicPath,
-    historyApiFallback: true,
-    host: '0.0.0.0',
-    port: process.env.DEV_SERVER_PORT,
-    proxy: {
-      '/api': `http://localhost:${process.env.PORT}`
-    },
-    stats: 'minimal',
-    watchContentBase: true
-  }
+  stats: 'minimal',
+  devtool: isDevelopment ? 'cheap-module-source-map' : 'source-map',
+  plugins: [
+    new webpack.EnvironmentPlugin([]),
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    isDevelopment && new webpack.NoEmitOnErrorsPlugin(),
+    isDevelopment && new webpack.HotModuleReplacementPlugin()
+  ].filter(Boolean)
 };
